dd9468a0477b9e4f84a2c14b9be2fd2ed49f7fc2
Theo B <theo.besson@gmx.fr>
2023-02-25 12:20:42 +0100

    quelques modif page d'accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { GetweatherComponent } from './getweather/getweather.component';
 import { ResultsComponent } from './results/results.component';
 import { FindbycityComponent } from './findbycity/findbycity.component';
 import { NewplaceComponent } from './newplace/newplace.component';
+import { MapBoxComponent } from './map-box/map-box.component';
 
 const routes: Routes = [
   { path: 'getweather', component: GetweatherComponent },
   { path: 'results/:data', component: ResultsComponent },
   { path: 'findbycity', component: FindbycityComponent },
   { path: 'newplace', component: NewplaceComponent },
+  { path: 'map', component: MapBoxComponent },
   // { path: '', redirectTo: '/getweather', pathMatch: 'full' },
 ];
 
